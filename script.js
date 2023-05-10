11352bff1c71215fb9daf95f09dcf8199a2a375f
DigambarGaikwad <104820408+DigambarGaikwad@users.noreply.github.com>
2023-05-10 12:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+const bandNames = ["The Beatles", "Aerosmith", "Red Hot Chili Peppers", "The Rolling Stones", "Guns N' Roses", "The Who", "Anthrax"];
+
+const sortedBandNames = bandNames.sort((a, b) => {
+  // Remove articles from band names for comparison
+  const bandA = a.replace(/^(a |an |the )/i, "");
+  const bandB = b.replace(/^(a |an |the )/i, "");
+  
+  return bandA.localeCompare(bandB); // Sort the band names lexicographically
+});
+
+const ul = document.getElementById("bands");
+
+sortedBandNames.forEach((band) => {
+  const li = document.createElement("li");
+  li.textContent = band;
+  ul.appendChild(li);
+});
